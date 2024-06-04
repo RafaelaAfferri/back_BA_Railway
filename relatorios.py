38f6a9f2562c2f5d69d8f2faed3aafd449420a7b
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2024-06-04 09:00:00 -0300

    feat(relatorio): :construction: ligacoes
    parte de ligacoes na tabela

--- relatorios.py
+++ relatorios.py
@@ -2,6 +2,7 @@ import pdfkit
 from jinja2 import Environment, FileSystemLoader
 import os
 
+
 def generate_pdf(context, template_path, css_path, output_path):
     template_dir = os.path.dirname(os.path.abspath(template_path))
     template_file = os.path.basename(template_path)
@@ -36,7 +37,22 @@ context = {
                  "Comprometeu-se a zelar pela frequência do estudante e manter o diálogo com a escola."),
     "assinatura_responsavel": "Antonia C. A. Costa",
     "data_assinatura": "Data assinatura",
-    "logo_path": "logo.png"
+    "logo_path": "logo.png",
+    # lista de ligacoes realizadas
+    "ligacoes": [
+        {
+            "responsavel": "João Paulo da Silva",
+            "data": "24/03/2024",
+            "telefone": "(11) XXXX-XXXX",
+            "observacoes": "Ligação não atendida, então mandei mensagem no Whatsapp"
+        },
+        {
+            "responsavel": "João Paulo da Silva",
+            "data": "26/03/2024",
+            "telefone": "(11) XXXX-XXXX",
+            "observacoes": "A mãe atendeu mas perdemos a conexão. Não consegui mais contato."
+        }
+    ]
 }
 
 
