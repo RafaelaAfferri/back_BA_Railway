6d3376a9951cea8207530aecc1d3c5b237c50eb0
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2025-01-20 17:59:51 -0300

    heroku

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import pymongo
 import certifi
 import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class CONFIG:
     JWT_SECRET_KEY = 'busca-ativa-escolar'
     JWT_VERIFY_EXPIRATION = True
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=10)
-    # MONGO_URI = 'mongodb+srv://admin:admin@cluster0.dc2vjrc.mongodb.net/buscaAtiva?retryWrites=true&w=majority'
-    MONGO_URI = 'mongodb+srv://admin:admin@ba.auiky.mongodb.net/buscaAtiva?retryWrites=true&w=majority'
+    MONGO_URI = load_dotenv("MONGO_URI")
 
 client_mongo = pymongo.MongoClient(CONFIG.MONGO_URI, tlsCAFile=certifi.where())
 accounts = client_mongo.buscaAtiva.accounts
